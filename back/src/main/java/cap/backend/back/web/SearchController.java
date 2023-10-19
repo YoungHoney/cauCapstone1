c6f9827addb42bb93d1ea98b231dadf1b76f2b13
Hcyo <hcyoon2000@gmail.com>
2023-10-19 19:03:02 +0900

    updated searchByInitial controller class

--- back/src/main/java/cap/backend/back/web/SearchController.java
+++ back/src/main/java/cap/backend/back/web/SearchController.java
@@ -1,17 +1,31 @@
 package cap.backend.back.web;
 
+import cap.backend.back.domain.Clan;
+import cap.backend.back.repository.PersonRepository;
+import cap.backend.back.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
 @Slf4j
 public class SearchController {
+    private final SearchService searchService;
     @PostMapping
     public String searchByName(@RequestParam String name, RedirectAttributes redirectAttributes){
+
         /*db에 있는 경우
         return "redirect:/ancestor/{id}";
          */
@@ -33,16 +47,19 @@ public class SearchController {
         return "hello";
     }
     @GetMapping("/initial/{letter}")
-    public String searchByInitial(@PathVariable String letter){
-        /*
-        해당하는 초성으로 시작하는 본관 목록 들고오기
-         */
-        log.info("letter={}", letter);
-        return "hello";
+    public CollectionModel<EntityModel<Clan>> searchByInitial(@PathVariable String letter){
+        List<EntityModel<Clan>> clans = searchService.findClansByLetter(letter).stream()
+                .map(clan -> EntityModel.of(clan,
+                        linkTo(methodOn(SearchController.class).searchByInitialClan(letter,
+                                clan.getClanid().getClanHangul()+clan.getClanid().getSurnameHangul()+"씨")).withSelfRel(),
+                        linkTo(methodOn(SearchController.class).searchByInitial(letter)).withRel("initial")))
+                        .toList();
+        return CollectionModel.of(clans, linkTo(methodOn(SearchController.class).searchByInitial(letter)).withSelfRel());
+
     }
     @GetMapping("/initial/{letter}/{clan}")
     //clan optional value로 만들어서 합치는 것도 가능할 듯
-    public String searchByInitial(@PathVariable String letter, @PathVariable String clan){
+    public String searchByInitialClan(@PathVariable String letter, @PathVariable String clan){
         /*
         해당하는 초성으로 시작하는 본관 목록 들고오기
          */
